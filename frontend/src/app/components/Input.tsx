f9522f448a134281d05ccdbdf89e2ec4dfb648bc
Goke Adekanye <adekanyeadegoke96@gmail.com>
2024-04-05 19:14:49 +0100

    refactored auth code

---
+++ frontend/src/app/components/Input.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface InputProps {
+  label: string;
+  name: string;
+  type: string;
+  required: boolean;
+  //   value: string;
+  //   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: React.FC<InputProps> = ({ label, name, type, required }) => {
+  return (
+    <div className="input-container relative w-full">
+      <input
+        className="peer block w-full rounded-t-lg border-0 bg-inherit px-25 pb-25
+        pt-5 text-sm text-gray-900"
+        name={name}
+        type={type}
+        required={required}
+      />
+      <label className="input-label">{label}</label>
+    </div>
+  );
+};
+
+export default Input;
