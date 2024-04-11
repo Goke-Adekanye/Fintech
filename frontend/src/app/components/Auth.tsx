2f0412e617c0861ea2341433040c3630604d6a67
Goke Adekanye <adekanyeadegoke96@gmail.com>
2024-04-11 16:13:32 +0100

    fix css structure

--- frontend/src/app/components/Auth.tsx
+++ frontend/src/app/components/Auth.tsx
@@ -35,106 +35,82 @@ const Auth: FC<AuthType> = ({
 }) => {
   const form = useRef<HTMLFormElement>(null);
   return (
-    <div className="auth">
-      <section className="auth-section grid-cols__3">
-        <div className="auth-section__first p-24 flex col-span__2 flex-col bg-home h-screen">
-          <div className="logo flex items-center justify-between">
-            <div className={`inline-block cursor-pointer`}>
-              <AccessLogo />
+    <section className="auth">
+      <div className="auth-section__first flex col-span__2 flex-col h-screen">
+        <div className="flex items-center justify-between">
+          <div className="inline-block cursor-pointer">
+            <AccessLogo />
+          </div>
+        </div>
+        <div className="form-area m-auto w-full">
+          <h1>Welcome to Access Internet Banking</h1>
+          <p className="text-xs">
+            Sign in with your Internet Banking details or Access More login
+            details. Not registered on Internet Banking or Access More? Click on
+            register to get started
+          </p>
+          <p className="text-xs">
+            {accountInfoText.initialText && (
+              <>
+                <span>No account?</span>
+                <Link className="auth-link text-xs font-bold" href="/sign-up">
+                  {" "}
+                  {accountInfoText.initialText}
+                </Link>
+              </>
+            )}
+          </p>
+          <form ref={form} onSubmit={(e) => onSubmit(e, form)}>
+            <div>
+              <div className="input-area flex items-center">
+                <section className="mr-3">
+                  <InputIcon changeIcon />
+                </section>
+                <Input label="Email" name="email" type="email" required />
+              </div>
+
+              <div className="input-area second flex items-center">
+                <section className="mr-3">
+                  <InputIcon />
+                </section>
+                <Input
+                  label="Password"
+                  name="password"
+                  type="password"
+                  required
+                />
+              </div>
+
+              <Link className="text-xs auth-link" href="/">
+                <span className="auth-link__span flex justify-end">
+                  Forgot Username or Password?
+                </span>
+              </Link>
             </div>
-          </div>
-          <div className="form-area m-auto w-full max-w-425px">
-            <h1 className="mb-2 text-28px">
-              Welcome to Access Internet Banking
-            </h1>
-            <p className="mb-6 text-xs">
-              Sign in with your Internet Banking details or Access More login
-              details. Not registered on Internet Banking or Access More? Click
-              on register to get started
-            </p>
-            <p className="mb-6 text-xs">
-              {accountInfoText.initialText && (
-                <>
-                  <span>No account?</span>
-                  <Link className="auth-link text-xs font-bold" href="/sign-up">
-                    {" "}
-                    {accountInfoText.initialText}
-                  </Link>
-                </>
-              )}
-            </p>
-            <form ref={form} onSubmit={(e) => onSubmit(e, form)}>
-              <div className="auth-inputs">
-                <div className="input-email">
-                  <div className="flex items-center px-3 py-1 rounded-md bg-white">
-                    <section className="mr-3">
-                      <InputIcon changeIcon />
-                    </section>
-                    <Input label="Email" name="email" type="email" required />
-                  </div>
-                </div>
 
-                <div className="input-password">
-                  <div className="flex items-center px-3 py-1 rounded-md bg-white">
-                    <section className="mr-3">
-                      <InputIcon />
-                    </section>
-                    <Input
-                      label="Password"
-                      name="password"
-                      type="password"
-                      required
-                    />
-                  </div>
-
-                  <Link
-                    className="text-xs auth-link hover:border-none"
-                    href="/"
-                  >
-                    <span className="mt-2 flex justify-end">
-                      {" "}
-                      Forgot Username or Password?
-                    </span>
-                  </Link>
-                </div>
-              </div>
-              <div className="auth-buttons">
-                <section className="auth-button__section">
-                  <button
-                    type="submit"
-                    disabled={loading}
-                    className="top-button"
-                  >
-                    {buttonTitle} {loading && "..."}
-                  </button>
-                </section>
-                <section className="auth-button__section second">
-                  <Link
-                    href={accountInfoText.actionLink}
-                    className="top-button"
-                  >
-                    {accountInfoText.actionText}
-                  </Link>
-                </section>
-              </div>
-            </form>
-          </div>
+            <div className="auth-buttons">
+              <section className="auth-button__section">
+                <button type="submit" disabled={loading} className="top-button">
+                  {buttonTitle} {loading && "..."}
+                </button>
+              </section>
+              <section className="auth-button__section second">
+                <Link href={accountInfoText.actionLink} className="top-button">
+                  {accountInfoText.actionText}
+                </Link>
+              </section>
+            </div>
+          </form>
         </div>
+      </div>
 
-        <div className="auth-section__second h-screen">
-          <div className="flex w-full items-center justify-center py-10">
-            <Image
-              width={333}
-              height={580}
-              src={src}
-              alt="banner"
-              className=""
-            />
-          </div>
-          <Image src={srcLine} alt="" className="line-image w-full" />
+      <div className="auth-section__second h-screen">
+        <div className="flex w-full items-center justify-center py-10">
+          <Image width={333} height={580} src={src} alt="banner" />
         </div>
-      </section>
-    </div>
+        <Image src={srcLine} alt="line" className="line-image w-full" />
+      </div>
+    </section>
   );
 };
 
--- frontend/src/app/components/Input.tsx
+++ frontend/src/app/components/Input.tsx
@@ -11,15 +11,14 @@ interface InputProps {
 
 const Input: React.FC<InputProps> = ({ label, name, type, required }) => {
   return (
-    <div className={`input-container email relative w-full`}>
+    <div className={`input-container relative w-full`}>
       <input
-        className="peer block w-full rounded-t-lg border-0 bg-inherit px-25 pb-25
-        pt-5 text-sm text-gray-900"
+        className="block w-full text-sm"
         name={name}
         type={type}
         required={required}
       />
-      <label className="input-label">{label}</label>
+      <label>{label}</label>
     </div>
   );
 };
